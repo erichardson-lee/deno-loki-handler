7847baf5b9da97c7f2ad14968cb959c955b80dbb
Edward Richardson-lee <erichardson-lee@fairfields.co.uk>
2023-01-10 10:56:48 +0000

    Run deno fmt

--- README.md
+++ README.md
@@ -1,27 +1,25 @@
 # Deno-Loki
 
-
-
 a simple to use [Loki](https://grafana.com/oss/loki/) logger for deno
 
-
-Please feel free to use the code in this project for your own projects this is more a example than anything useful
+Please feel free to use the code in this project for your own projects this is
+more a example than anything useful
 
 ```ts
-import { LokiLogger } from 'https://deno.land/x/loki/mod.ts';
+import { LokiLogger } from "https://deno.land/x/loki/mod.ts";
 let logger = new LokiLogger(
-	'http://localhost:3100',
-	1,
-	{
-		host: 'localhost',
-		job: 'Deno!',
-	},
-	'TEXT'
+  "http://localhost:3100",
+  1,
+  {
+    host: "localhost",
+    job: "Deno!",
+  },
+  "TEXT",
 );
-logger.info('Some info', { data: 'Some more info' });
-logger.warm('Some info', { data: 'Some more info' });
-logger.debug('Some info', { data: 'Some more info' });
-logger.error('Some info', { data: 'Some more info' });
+logger.info("Some info", { data: "Some more info" });
+logger.warm("Some info", { data: "Some more info" });
+logger.debug("Some info", { data: "Some more info" });
+logger.error("Some info", { data: "Some more info" });
 ```
 
 ![img](images/NQz4MDp.png)
--- mod.ts
+++ mod.ts
@@ -1,14 +1,14 @@
-import { RangeOf } from './rangeof.ts';
+import { RangeOf } from "./rangeof.ts";
 
 type Entry = [string, string];
 
 interface Body {
-	streams: [
-		{
-			stream: Record<string, string>;
-			values: Entry[];
-		}
-	];
+  streams: [
+    {
+      stream: Record<string, string>;
+      values: Entry[];
+    },
+  ];
 }
 
 /**
@@ -19,76 +19,74 @@ interface Body {
  *  @param mode mode to post
  */
 export class LokiLogger {
-	toReport?: Entry[];
-	constructor(
-		public url: string = 'http://localhost:3100',
-		public buffer: RangeOf<50> = 1,
-		public labels: Record<string, string> = {
-			host: 'localhost',
-			job: 'Deno!',
-		},
-		public mode: 'JSON' | 'TEXT' = 'TEXT'
-	) {
-		if (buffer !== 1) {
-			this.toReport = [];
-		}
-	}
-	private async pushPayload(entry: Entry) {
-		if (this.buffer !== 1) this.toReport!.push(entry);
-		if (this.buffer === this.toReport?.length) {
-			await this.report(this.createPayload(this.toReport));
-			//Clear the reports
-			this.toReport.length = 0;
-		} else if (this.buffer === 1) {
-			await this.report(this.createPayload([entry]));
-		}
-	}
-	private createPayload(entries: Entry[]): Body {
-		return {
-			streams: [{ stream: this.labels, values: entries }],
-		};
-	}
-	private async report(body: Body) {
-		//https://github.com/grafana/loki/issues/173
-		//https://github.com/sleleko/devops-kb/blob/master/python/push-to-loki.py
-		let r = await fetch(`${this.url}/loki/api/v1/push`, {
-			headers: { 'Content-type': 'application/json' },
-			method: 'POST',
-			body: JSON.stringify(body),
-		});
-		if (!r.ok) {
-			throw new Error(
-				`FAILED TO POST DATA ${await r.text()} ${JSON.stringify(r)}`
-			);
-		}
-	}
+  toReport?: Entry[];
+  constructor(
+    public url: string = "http://localhost:3100",
+    public buffer: RangeOf<50> = 1,
+    public labels: Record<string, string> = {
+      host: "localhost",
+      job: "Deno!",
+    },
+    public mode: "JSON" | "TEXT" = "TEXT",
+  ) {
+    if (buffer !== 1) {
+      this.toReport = [];
+    }
+  }
+  private async pushPayload(entry: Entry) {
+    if (this.buffer !== 1) this.toReport!.push(entry);
+    if (this.buffer === this.toReport?.length) {
+      await this.report(this.createPayload(this.toReport));
+      //Clear the reports
+      this.toReport.length = 0;
+    } else if (this.buffer === 1) {
+      await this.report(this.createPayload([entry]));
+    }
+  }
+  private createPayload(entries: Entry[]): Body {
+    return {
+      streams: [{ stream: this.labels, values: entries }],
+    };
+  }
+  private async report(body: Body) {
+    //https://github.com/grafana/loki/issues/173
+    //https://github.com/sleleko/devops-kb/blob/master/python/push-to-loki.py
+    let r = await fetch(`${this.url}/loki/api/v1/push`, {
+      headers: { "Content-type": "application/json" },
+      method: "POST",
+      body: JSON.stringify(body),
+    });
+    if (!r.ok) {
+      throw new Error(
+        `FAILED TO POST DATA ${await r.text()} ${JSON.stringify(r)}`,
+      );
+    }
+  }
 
-	public log(label: string, text: string, meta?: Record<string, string>) {
-		meta = {
-			label,
-			text,
-			...meta,
-		};
-		return this.pushPayload([
-			`${Date.now()}000000`,
-			this.mode == 'JSON'
-				? JSON.stringify(meta)
-				: Object.entries(meta)
-						.map(([k, v]) => `${k}="${v}"`)
-						.join(' '),
-		]);
-	}
+  public log(label: string, text: string, meta?: Record<string, string>) {
+    meta = {
+      label,
+      text,
+      ...meta,
+    };
+    return this.pushPayload([
+      `${Date.now()}000000`,
+      this.mode == "JSON" ? JSON.stringify(meta) : Object.entries(meta)
+        .map(([k, v]) => `${k}="${v}"`)
+        .join(" "),
+    ]);
+  }
 
-	public info(text: string, meta?: Record<string, string>) {
-		return this.log('INFO', text, meta);
-	}
-	public warm(text: string, meta?: Record<string, string>) {
-		return this.log('WARN', text, meta);
-	}
-	public error(text: string, meta?: Record<string, string>) {
-		return this.log('ERROR', text, meta);
-	}
-	public debug(text: string, meta?: Record<string, string>) {
-		return this.log('DEBUG', text, meta);
-	}
+  public info(text: string, meta?: Record<string, string>) {
+    return this.log("INFO", text, meta);
+  }
+  public warm(text: string, meta?: Record<string, string>) {
+    return this.log("WARN", text, meta);
+  }
+  public error(text: string, meta?: Record<string, string>) {
+    return this.log("ERROR", text, meta);
+  }
+  public debug(text: string, meta?: Record<string, string>) {
+    return this.log("DEBUG", text, meta);
+  }
 }
